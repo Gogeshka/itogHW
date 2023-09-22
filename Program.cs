f8b9459dbcd4fe5f6dcf14783eba07a2094dc7ca
EgorT <terrragoga@gmail.com>
2023-09-22 17:10:30 +0500

    Сделал дз

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arrayA = new string[5] {"111", "2222", "3", "44", "55555"};
+string[] arrayB = new string[arrayA.Length];
+void SArray(string[] arrayA, string[] arrayB)
+{
+    int count = 0;
+    for (int i = 0; i < arrayA.Length; i++)
+    {
+    if(arrayA[i].Length <= 3)
+        {
+        arrayB[count] = arrayA[i];
+        count++;
+        }
+    }
+}
+void PrA(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SArray(arrayA, arrayB);
+PrA(arrayB);
